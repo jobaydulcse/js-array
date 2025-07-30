6c1a225a04b2f2479269f4056a2ca88dad6c4022
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-31 02:28:26 +0600

    pop added

---
+++ pop.js
@@ -0,0 +1,10 @@
+const friends = ["balam", "kalam", "salam", "pailam", "gelam"];
+
+// friends.push("talam");
+console.log(friends);
+
+const out1 = friends.pop();
+console.log(friends);
+const out2 = friends.pop();
+console.log(friends);
+console.log(out1, out2);
