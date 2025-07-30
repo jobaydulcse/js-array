7b1d1a7d8cb8262aa7b5284598f67a788d158581
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-31 02:34:03 +0600

    shift-unshift added

---
+++ shift-unshift.js
@@ -0,0 +1,8 @@
+const friends = ["balam", "kalam", "salam", "pailam", "gelam"];
+
+console.log(friends);
+friends.shift();
+console.log(friends);
+
+friends.unshift("ghumailam");
+console.log(friends);
